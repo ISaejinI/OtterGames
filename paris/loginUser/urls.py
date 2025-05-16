13b4fcefa408b7836cc83ac18378c438263def61
Lou-Anne <saejinmx@gmail.com>
2025-05-16 11:43:21 +0200

    Inscription fonctionel

--- paris/loginUser/urls.py
+++ paris/loginUser/urls.py
@@ -5,4 +5,6 @@ urlpatterns=[
     path('login/',views.login_page,name='login'),
     path('logout/',views.logout_user,name='logout'),
     path('profil/', views.userAccount,name='profil'),
+    path('signup/', views.signup_page,name='signup'),
+    path('', views.home_page, name='home'),
 ]
--- paris/loginUser/views.py
+++ paris/loginUser/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 from . import forms
 
 def login_page(request):
@@ -24,6 +25,21 @@ def login_page(request):
 def logout_user(request):
     logout(request)
     return redirect('login')
+
+
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'signup.html', context={'form': form})
+
+
+def home_page(request):
+    return render(request, 'home.html')
 
 
 @login_required
