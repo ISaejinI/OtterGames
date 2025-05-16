53c6f010c9925585438478c7fadd851dc48b95f3
Lou-Anne <saejinmx@gmail.com>
2025-05-16 16:20:41 +0200

    Début d'affichage d'un match

--- paris/tournois/views.py
+++ paris/tournois/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Match
+from .models import Match, Tournoi, Loutre
 
 def match_page(request, match_id):
     match = Match.objects.get(pk = match_id)
+    tournoi = Tournoi.objects.get(pk = match.tournoi_id)
+    loutre1 = Loutre.objects.get(pk = match.loutre1_id)
+    loutre2 = Loutre.objects.get(pk = match.loutre2_id)
 
-
-
-
-    return render(request, 'match.html', {'match': match})
+    return render(request, 'match.html', {'match': match, 'tournoi': tournoi, 'loutre1': loutre1, 'loutre2': loutre2})
